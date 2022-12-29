bce6005d47964d9e93a145ea3537273b3623d9e7
MrGandhe <100147587+MrGandhe@users.noreply.github.com>
2022-12-29 19:02:22 +0530

    update

--- module/i_am_role.tf
+++ module/i_am_role.tf
@@ -41,7 +41,36 @@ resource "aws_iam_policy" "policy" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "test-attach" {
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "${var.lambda_function_name}lambda_logging_policy"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-logs-role-attach" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "basic-lambdaexecution-role-attach" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.policy.arn
 }
