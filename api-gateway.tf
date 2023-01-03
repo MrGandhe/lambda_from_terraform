db8474d2e1ad5761b73d9fa15a662270c8de64e3
sparshbaurasi <107546419+sparshbaurasi@users.noreply.github.com>
2023-01-03 14:02:56 +0530

    Add files via upload

---
+++ api-gateway.tf
@@ -0,0 +1,59 @@
+resource "aws_apigatewayv2_api" "main" {
+  name          = "main"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "dev" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  name        = "dev"
+  auto_deploy = true
+
+
+}
+
+resource "aws_cloudwatch_log_group" "main_api_gw" {
+  name = "/aws/api-gw/${aws_apigatewayv2_api.main.name}"
+
+  # retention_in_days = 14
+}
+
+resource "aws_apigatewayv2_integration" "lambda_hello" {
+  api_id = aws_apigatewayv2_api.main.id
+  depends_on = [
+    module.labmda-in-vpc
+  ]
+  integration_uri = module.labmda-in-vpc["lambda156"].lambda_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "get_hello" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_hello.id}"
+}
+
+resource "aws_apigatewayv2_route" "post_hello" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  route_key = "POST /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_hello.id}"
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+   depends_on = [
+    module.labmda-in-vpc
+  ]
+  action        = "lambda:InvokeFunction"
+  function_name = module.labmda-in-vpc["lambda156"].lambda_arn
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
+
+output "hello_base_url" {
+  value = aws_apigatewayv2_stage.dev.invoke_url
+}
